89c1294040cd3c8d765167de6b4f302dab0ab41b
Nikita Vikulin <vikulin-na@ic.ntcees.ru>
2024-10-21 19:24:01 +0300

    Добавлены фикстуры

--- helpers/account_helper.py
+++ helpers/account_helper.py
@@ -14,7 +14,7 @@ def retry_if_result_none(
 
 
 @retry(stop_max_attempt_number=6)
-def stop_after_5_attempts():
+def stop_after_6_attempts():
     print("Stopping after 6 attempts")
 
 
@@ -27,6 +27,16 @@ class AccountHelper:
     ):
         self.dm_account_api = dm_account_api
         self.mailhog = mailhog
+
+    def auth_client(self, login: str, password: str):
+        response = self.dm_account_api.login_api.post_v1_account_login(
+            json_data={"login": login, "password": password}
+        )
+        token = {
+            "x-dm-auth-token": response.headers["x-dm-auth-token"]
+        }
+        self.dm_account_api.account_api.set_headers(token)
+        self.dm_account_api.login_api.set_headers(token)
 
     def register_new_user(
             self,
